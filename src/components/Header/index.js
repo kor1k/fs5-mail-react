9e98684e4ba756024cf7777af0e7179eeac1482d
kor1k <koortttt@gmail.com>
2019-02-18 21:24:15 +0200

    adding some design3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-4">Logo</div>
+                    <div className="col-4 gmail-logotype"><img width='130px' height='70px' src="https://imageog.flaticon.com/icons/png/512/281/281769.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt=""/></div>
                     <div className="col-4"><SearchBar /></div>
                     <div className="col-4">
                         <NavLink to="/" activeClassName="active">Mail</NavLink>
